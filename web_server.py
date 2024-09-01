50ddfa5c01dbc132fdd3517729042a72209811d1
xdetre <enderctoun@gmail.com>
2024-09-01 14:16:52 +0300

    Changed

--- web_server.py
+++ web_server.py
@@ -43,6 +43,6 @@ def after_request(response):
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=False)
 
-# something
+# something aaa
 
 # https://wexler.io/course/coding/todo-frontend/
